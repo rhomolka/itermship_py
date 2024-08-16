b246b5e0920936665c800281d72f3d4dcf9b440a
Richard Homolka <77993544+rhomolka@users.noreply.github.com>
2024-08-16 00:16:28 -0500

    Cleaner git, initial iTerm2 config

---
+++ CONFIGURE.md
@@ -0,0 +1,16 @@
+## Enable Status Bar
+Go to `Settings...` => `Profiles` => `(Profile name)` => `Session` Tab => Check `Status bar enabled`
+
+## Configure Status Bar Appearance
+Go to `Settings...` => `Profiles` => `(Profile name)` => `Session` Tab => Click `Configure Status Bar`
+
+Click `Advanced...` at the bottom, change the Font to a NerdFont (e.g. Hack-NF-Regular)
+
+Add a new component 'Interpolated String' and configure it.  e.g. for the git component:  
+
+* Add an 'Interpolated String'
+* Select the new component.  Click `Configure Component`
+* for String Value: add `\(user.git)` as a user variable
+
+## Configure Status Bar Location
+Go to `Settings...` => `Appearance` => `General` Tab.  In `Status bar location` select `Bottom`
--- lib/plugin/Git/__init__.py
+++ lib/plugin/Git/__init__.py
@@ -1,5 +1,5 @@
 
-import subprocess
+import subprocess, re
 
 giticon = ''
 def getItermData():
@@ -7,59 +7,64 @@ def getItermData():
     if cmdout.returncode != 0:
         return ['git', '']
     
-    newfile = 0
-    indexchange = 0
-    workingtree = 0
-    unknownfile = 0
-    indexstring = ''
-    workingstring = ''
-    unknownstring = ''
+    newfile_ct = 0
+    indexchange_ct = 0
+    workingtree_ct = 0
+    unknownfile_ct = 0
+    index_str = ''
+    working_str = ''
+    unknown_str = ''
     branch = ''
-    aheadstring = ''
-    behindstring = ''
-    for line in cmdout.stdout.splitlines(False):
-        if line[0:2] == '##':
-            if '(no branch)' in line:
-                branch = '(no branch)'
-            elif 'No commits yet on ' in line[3:]:
-                branch = line[21:]
-            else:
-                restofline = line[3:]
-                aheadblock = ''
-                
-                try:
-                    aheadindex = restofline.index('[')
-                except:
-                    pass
-                else:
-                    aheadblock = restofline[aheadindex:]
-                    restofline = restofline[0:aheadindex - 1]
-                
-                try:
-                    index = restofline.index('.')
-                except:
-                    pass
-                else:
-                    # we don't care about remote branch, yet
-                    restofline = restofline[0:index]
-                branch = restofline
-        elif line[0:1] == 'A' or line[1:1] == 'A':
-            newfile += 1
-        elif line[0:1] in 'CDTMRU':
-            indexchange += 1
-        elif line[1:2] in 'CDTMRU':
-            workingtree += 1
-        elif line[0:2] == '??':
-            unknownfile += 1
+    ahead_ct = 0
+    behind_ct = 0
+    ahead_str = ''
+    behind_str = ''
+    
+    stdoutlines = cmdout.stdout.splitlines(False)
+    firstline = stdoutlines.pop(0)
+ 
+    if '(no branch)' in firstline:
+        branch = '(no branch)'
+    elif 'No commits yet on ' in firstline[3:]:
+        branch = firstline[21:]
+    else:
+        m1 = re.search('^## ([a-z][^. ]*)', firstline)
+        if m1 is not None:
+            branch = m1.group(1)
+        else:
+            raise Exception("Assert: Bad regex")
+    
+    ahead_match = re.search('ahead ([1-9][0-9]*)', firstline)
+    if ahead_match is not None:
+        ahead_ct = int(ahead_match.group(1))
+    behind_match = re.search('behind ([1-9][0-9]*)', firstline)
+    if behind_match is not None:
+        behind_ct = int(behind_match.group(1))
+        
 
-    newfilestring = ''
-    if newfile > 0:
-        newfilestring = f'  {newfile}'
-    if indexchange > 0:
-        indexstring = f'  {indexchange}'
-    if workingtree > 0:
-        workingstring = f'  {workingtree}'
-    if unknownfile > 0:
-        unknownstring = f' 󱀶 {unknownfile}'
-            
-    return ["git",f"{giticon} {branch}{indexstring}{workingstring}{newfilestring}{unknownstring}{aheadstring}{behindstring}"]
+    # we did the first line, now the rest
+    for line in stdoutlines:
+        if line[0:1] == 'A' or line[1:1] == 'A':
+            newfile_ct += 1
+        elif line[0:1] in 'CDTMRU':
+            indexchange_ct += 1
+        elif line[1:2] in 'CDTMRU':
+            workingtree_ct += 1
+        elif line[0:2] == '??':
+            unknownfile_ct += 1
+
+    newfile_str = ''
+    if newfile_ct > 0:
+        newfile_str = f'  {newfile_ct}'
+    if indexchange_ct > 0:
+        index_str = f'  {indexchange_ct}'
+    if workingtree_ct > 0:
+        working_str = f'  {workingtree_ct}'
+    if unknownfile_ct > 0:
+        unknown_str = f' 󱀶 {unknownfile_ct}'
+    if ahead_ct > 0:
+        ahead_str = f' 󰁞{ahead_ct}'
+    if behind_ct > 0:
+        behind_str = f' 󰁆{behind_ct}'
+
+    return ["git",f"{giticon} {branch}{index_str}{working_str}{newfile_str}{unknown_str}{ahead_str}{behind_str}"]

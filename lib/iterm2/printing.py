4b85ad630e1dc411b06aaef6b2663cf7d5a0e494
Richard Homolka <77993544+rhomolka@users.noreply.github.com>
2024-09-04 17:03:44 -0500

    More descriptive name

--- lib/iterm2/printing.py
+++ lib/iterm2/printing.py
@@ -1,6 +1,6 @@
 import base64
 
-def dump(data):
+def dumpAsItermData(data):
     encodedData = base64.b64encode(data[1].encode('utf-8')).decode('ascii')
     userdata = f']1337;SetUserVar={data[0]}={encodedData}'
     string = "\033" + userdata + "\007"
